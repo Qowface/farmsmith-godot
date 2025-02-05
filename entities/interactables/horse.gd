4427b278fcf2110e85e416c51845dba44d562e6e
Robert Marquess <qowsrule@gmail.com>
2025-02-05 00:51:43 -0500

    Add crop resource

--- entities/interactables/horse.gd
+++ entities/interactables/horse.gd
@@ -3,6 +3,12 @@ extends StaticBody2D
 
 @export var dialogue_starting: Dialogue
 
+var given_carrot_seed: bool = false
+
 
 func interact():
-	GlobalSignals.show_dialogue.emit(dialogue_starting)
+	if !given_carrot_seed:
+		GlobalSignals.show_dialogue.emit(dialogue_starting)
+		given_carrot_seed = true
+	else:
+		GlobalSignals.show_text_box.emit("neigh!")
---
+++ resources/crops/crop.gd
@@ -0,0 +1,9 @@
+class_name Crop
+extends Resource
+
+
+@export var name: String = ""
+@export var seed_price: int = 0
+@export var crop_price: int = 0
+@export var seed_sprite: CompressedTexture2D
+@export var crop_sprite: CompressedTexture2D
